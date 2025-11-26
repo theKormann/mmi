186de1ebb838351f829d5a548d1cd77fb96d5615
unknown <kormannmatheus@gmail.com>
2025-11-26 15:08:35 -0300

    add clauseDTO for backend

--- backend/src/main/java/com/mmi/models/dto/ClauseDTO.java
+++ backend/src/main/java/com/mmi/models/dto/ClauseDTO.java
@@ -1,8 +1,12 @@
 package com.mmi.models.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClauseDTO {
     private String title;
     private String content;
